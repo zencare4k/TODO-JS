4266e1730462b1d0794a0aedffe3a6d9f73b1f67
Carlos Marban <152007178+zencare4k@users.noreply.github.com>
2025-01-07 15:38:48 +0100

    xd

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+const TextField = document.getElementById("TextField");
+const container = document.getElementById("checkboxcontainer");
+
 function AddNote() {
-    const textField = document.getElementById("TextField")
-    const TextValue = textField.value
-    document.getElementById("List").innerHTML = `
-    <input type="checkbox" id="checkbox"> ${TextValue} `
+    const TextValue = TextField.value;
+
+    const input = document.createElement('input')
+    input.type = 'checkbox'
+
+    const label = document.createElement('label')
+    label.textContent = TextValue + "\n"
+
+    const editButton = document.createElement('button')
+    editButton.id = "editButton"
+    editButton.textContent = "Edit"
+    
+    label.appendChild(input);
+    
+
+    container.appendChild(label);
+
+
 }
